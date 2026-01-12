48ff8506b46a4fad41f9eee1d235f8722cc87875
TD green library <talaridinesh2005@gmail.com>
2026-01-12 07:21:35 +0530

    Update bypasser.py

--- bypasser.py
+++ bypasser.py
@@ -52,6 +52,7 @@ otherslist = [
     "social-unlock.com",
     "boost.ink",
     "goo.gl",
+    "gplinks.com",
     "shrto.ml",
     "t.co",
 ]
@@ -2794,3 +2795,36 @@ def shortners(url):
 
 
 ################################################################################################################################
+
+
+import time
+import cloudscraper
+from bs4 import BeautifulSoup
+
+import requests
+# eg: https://gplinks.co/XXXX
+url = "https://gplinks.co/Z94r6"
+
+# =======================================
+
+def gplinks_bypass(url: str):
+ client = cloudscraper.create_scraper(allow_brotli=False)  
+ domain ="https://gplinks.co/"
+ referer = "https://mynewsmedia.co/"
+
+ vid = client.get(url, allow_redirects= False).headers["Location"].split("=")[-1]
+ url = f"{url}/?{vid}"
+
+ response = client.get(url, allow_redirects=False)
+ soup = BeautifulSoup(response.content, "html.parser")
+    
+    
+ inputs = soup.find(id="go-link").find_all(name="input")
+ data = { input.get('name'): input.get('value') for input in inputs }
+    
+ time.sleep(10)
+ headers={"x-requested-with": "XMLHttpRequest"}
+ bypassed_url = client.post(domain+"links/go", data=data, headers=headers).json()["url"]
+ return bypassed_url
+
+print(gplinks_bypass(url))
